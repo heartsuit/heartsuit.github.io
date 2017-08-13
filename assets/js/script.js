30a7075758e1d306a4657e21b8b857ede07fd370
mvvp <nxq0108@126.com>
2017-08-13 18:44:41 +0800

    test background

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+$.ajax({  
+  // url: 'https://3ae3192a.ngrok.io/background',
+  url: 'http://dataapi.ngrok.cc/background',
+  type: "GET",
+  dataType: "jsonp",
+  jsonpCallback: 'jsonpCallback',
+  data: {},
+  error: function (err) {
+    console.log(err.status);
+    console.log(err.readyState);
+    console.log(err.responseText);
+    console.log(err.statusText);
+  },
+  success: function (data) {
+    console.log(data);
+    $("body").css('background-size', `cover`);
+    $("body").css('background-image', `url(${data.background})`);
+    $("body").css('background-repeat', `no-repeat`);
+    
+  }
+});
+
+// var request = window.superagent;
+// request.get('http://cn.bing.com').end(function(err, res){
+//   if(err) {
+//     console.log(err);
+//   }
+//   console.log(res);
+// });
